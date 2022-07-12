c500dc4cb87e658bdd1a5f163e6bbbf48deea738
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-07-12 18:58:09 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const router = require("./routes/book-routes");
+//const router = require("./routes/book-routes");
 const app = express();
 
-app.use(express.json());
-app.use("/books",router)
+//app.use(express.json());
+//app.use("/books",router)
 
 mongoose.connect
 (
-    "mongodb+srv://admin:1234@cluster0.oyxgo.mongodb.net/book-Store?retryWrites=true&w=majority"
+    "mongodb+srv://admin:1234@cluster0.y4hf0.mongodb.net/book-store?retryWrites=true&w=majority"
 )
 
 .then( () => console.log("Connected To Database"))
