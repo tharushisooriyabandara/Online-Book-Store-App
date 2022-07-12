ba0e0c0d5c08ab9a1baa61c0e40b89a16e836003
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-07-12 21:52:22 +0530

    updated

--- frontend/book-store/src/components/Header.js
+++ frontend/book-store/src/components/Header.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import { AppBar , Typography } from '@mui/material';
 import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return <div>
-        <AppBar position='sticky'>
-            <toolbar>
-
+    const [value, setValue] = useState();
+    return 
+    <div>
+        <AppBar sx={{ backgroundColor: "#232F3D" }} position="sticky">
+        <Toolbar>
+          <NavLink to="/" style={{ color: "white" }}>
             <Typography>
-                <LibraryBooksOutlinedIcon />
+              <LibraryBooksOutlinedIcon />
             </Typography>
-
-            </toolbar>
-        
-
-        </AppBar>
+          </NavLink>
+          <Tabs
+            sx={{ ml: "auto" }}
+            textColor="inherit"
+            indicatorColor="primary"
+            value={value}
+            onChange={(e, val) => setValue(val)}
+          >
+            <Tab LinkComponent={NavLink} to="/add" label="Add product" />
+            <Tab LinkComponent={NavLink} to="/books" label="Books" />
+            <Tab LinkComponent={NavLink} to="/about" label="About Us" />
+          </Tabs>
+        </Toolbar>
+      </AppBar>
     </div>;
 
 };
