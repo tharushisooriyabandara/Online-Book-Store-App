c9f915b88888b04d37bb6ae2886bd3fdaa2d1064
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-07-12 22:05:37 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-//const router = require("./routes/book-routes");
+const router = require("./routes/book-routes");
+const cors = require("cors");
 const app = express();
 
-//app.use(express.json());
-//app.use("/books",router)
+app.use(express.json());
+app.use(cors());
+app.use("/books",router)
 
 mongoose.connect
 (
