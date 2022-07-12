d7e515fa89740ae6ee67808ddc2ffe7bb55cb067
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2022-07-12 18:51:40 +0530

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const app = express();
+
+app.use(express.json());
+app.use("/books",router)
+
+mongoose.connect
+(
+    "mongodb+srv://admin:1234@cluster0.oyxgo.mongodb.net/book-Store?retryWrites=true&w=majority"
+)
+
+.then( () => console.log("Connected To Database"))
+.then( () => 
+{
+    app.listen(5000);
+}).catch( (err) => console.log(err));
